420eb05bf0d6abff506f0b2cf73c00e92660419e
Eduardo <jeduardorjx@gmail.com>
2019-10-19 16:52:15 -0600

    Set Name component and name text into place

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,22 @@
-import React from 'react';
-import logo from '../logo.svg';
-import './App.scss';
+import React, { Component } from 'react'
+import './App.scss'
+import Name from '../Name/Name'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render = () => {
+    return (
+      <main>
+        <Name />
+      </main>
+    )
+  }
 }
 
 export default App;
-
-
---
+++ src/Name/Name.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Name.scss'
+
+const Name = () => {
+  return (
+    <div className='name-wrapper'>
+      <h1 className='name'>J. Eduardo Rodriguez</h1>
+    </div>
+  )
+}
+
+export default Name;
