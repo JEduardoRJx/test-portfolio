d1e6d41969fb0d262760f00473b41cd21ad3c4cc
Eduardo <jeduardorjx@gmail.com>
2019-10-19 17:20:53 -0600

    Add software developer tag and change styling for placement

--- src/Name/Name.js
+++ src/Name/Name.js
@@ -5,6 +5,7 @@ const Name = () => {
   return (
     <div className='name-wrapper'>
       <h1 className='name'>J. Eduardo Rodriguez</h1>
+      <h2 className='name-software-developer'>Software Developer</h2>
     </div>
   )
 }
