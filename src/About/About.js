d34d93f6296e53b3be60a191537cfb399f74f9c8
Eduardo <jeduardorjx@gmail.com>
2019-12-30 14:35:41 -0700

    Change about me test

--- src/About/About.js
+++ src/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <div className='description-wrapper'>
 
         <p>Hey There!</p>
-        <p>Having 5+ years of public library experience I want to transfer and adapt my skills and experiences to software development and continue helping people in our community.</p>
+        <p>I’m a software developer with an emphasis on front end technologies, resourceful problem solving, and collaboration. I look forward to working on a friendly, user focused team building software applications that are accessible and easy to use.</p>
       </div>
     </div>
   )
