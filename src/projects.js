f8704bbe96e17e187130ad35a6fe56525fbab183
Eduardo <jeduardorjx@gmail.com>
2019-12-30 19:05:43 -0700

    Add project information needed for the project cards

---
+++ src/projects.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    image: 'https://i.ibb.co/b11QcZH/Harry-Potter-Main-Page.png',
+    title: 'Harry Potter and the Sorting Game',
+    description: 'A web based application/game that allows Wizards and Witches to test their knowledge on characters and their respective houses!',
+    app: 'https://hp-sorting-game.herokuapp.com',
+    repo: 'https://github.com/JEduardoRJx/hp-sorting-game',
+  },
+  {
+    image: 'https://i.ibb.co/G2cyk1m/whats-new.png',
+    title: "What's New",
+    description: 'A news article displaying app based off a few categories such as local news, technology, entertainment, science, and health.',
+    app: 'https://jeduardorjx.github.io/whats-new/',
+    repo: 'https://github.com/JEduardoRJx/whats-new',
+  },
+  {
+    image: 'https://i.ibb.co/CKPG4bQ/Screen-Shot-2019-12-30-at-6-51-03-PM.png',
+    title: 'Star Wars Trivia',
+    description: 'The app displays Star Wars movies, characters, a log in, and a favorites page, each with their own sets of data.',
+    app: 'https://n-gibson.github.io/swapi-trivia/#/',
+    repo: 'https://github.com/JEduardoRJx/swapi-trivia',
+  }
+]
